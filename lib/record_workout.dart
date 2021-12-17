3f8f273687a87ed81e41dacbaa8d20da44db18f5
Hichem Chekir <70571974+HichemChekir@users.noreply.github.com>
2021-12-17 22:50:55 +0100

    trying to add the inputs

--- lib/record_workout.dart
+++ lib/record_workout.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:intl/intl.dart';
+import 'package:flutter_spinbox/material.dart';
 
 class record_workout extends StatefulWidget {
   const record_workout({Key key}) : super(key: key);
@@ -37,6 +39,7 @@ class _record_workoutState extends State<record_workout> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            Text('Bench Press'),
             Text(_dateTime == null
                 ? 'Nothing has been picked yet'
                 : formatter.format(_dateTime)),
@@ -59,7 +62,42 @@ class _record_workoutState extends State<record_workout> {
                   },
                 );
               },
-            )
+            ),
+            Row(
+              children: [
+                RaisedButton(
+                  onPressed: () {},
+                  shape: CircleBorder(side: BorderSide.none),
+                  child: Text(
+                    '+',
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                RaisedButton(
+                  onPressed: () {},
+                  shape: CircleBorder(side: BorderSide.none),
+                  child: Text(
+                    '-',
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Text('Set 1: '),
+                SizedBox(
+                  height: 51,
+                  width: 50,
+                  child: SpinBox(
+                    min: 0,
+                    max: 10,
+                    decoration: InputDecoration(labelText: 'Basic'),
+                    onChanged: (value) => print(value),
+                  ),
+                )
+              ],
+            ),
           ],
         ),
       ),
