c45f334f599ad673fef378ce00e55ff700466027
HichemChekir <hichem.chekir0@gmail.com>
2021-12-18 13:14:01 +0100

    faza

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
+import 'package:athletto/record_page.dart';
+import 'package:athletto/workouts_page1.dart';
 import 'package:flutter/material.dart';
 import 'home_page_modified.dart';
 import 'sign_in.dart';
 import 'record_workout.dart';
-
+import 'sign_up.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: record_workout(),
+    home: record_page(),
   ));
 }
 
